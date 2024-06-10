7da89fcbef1909d05ab82e91ab5c41deb455d81e
suraj <suraj.gupta@paymentz.com>
2024-06-10 19:26:17 +0530

    code push 10/06/2024

---
+++ src/Components/Offers/Offers.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Offers.css'
+import exclusive_image from '../Assests/exclusive_image.png'
+
+const Offers = () => {
+    return (
+        <div className='offers'>
+            <div className="offer-left">
+                <h1>Exclusive</h1>
+                <h1>Offer For you</h1>
+                <p>Only On Best Sellers Products</p>
+                <button>Check Now</button>
+            </div>
+            <div className="offer-right">
+                <img src={exclusive_image} alt=''/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Offers
